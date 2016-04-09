9442e992944c409fe719d5b1246b65152052ca34
Petter Jhunior Garcia Infante <petter_junior94@hotmail.com>
2016-04-09 06:25:26 +0200

    Agregado producto de frond end

---
+++ demoGIT/Controllers/ProductoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace demoGIT.Controllers
+{
+    public class ProductoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ demoGIT/Models/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace demoGIT.Models
+{
+    public class Producto
+    {
+        public int codigoproducto { get; set; }
+        public string nombreproducto { get; set; }
+        public string descripcion { get; set; }
+        public decimal precioproducto { get; set; }
+    }
+}
